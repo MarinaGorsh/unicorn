3104e32bc99b92808512de74c0a8ce26436701f2
Maryna Horshevska <69041379+MarinaGorsh@users.noreply.github.com>
2024-03-20 21:05:56 +0100

    Update tests.py

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
+from accounts.models import CustomUser
 
-# Create your tests here.
+class HomeViewTest(TestCase):
+    def test_home_view(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "Welcome to Unicorn AI!")
+        self.assertContains(response, "Log in")
+        self.assertContains(response, "Search profiles:")
+        self.assertContains(response, "Username:")
+
+class ResumeCreateViewTest(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(username='user', password='password')
+
+    def test_user_change_and_resume_create(self):
+        self.client.login( username='user', password='password')
+        response = self.client.get(reverse("user_change", args=["user"]))
+        self.assertContains(response, "Change info")
+        response2 = self.client.get(reverse("resume_create", args=["user"]))
+        self.assertContains(response2, "Create resume")
